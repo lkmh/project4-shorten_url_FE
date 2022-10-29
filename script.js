c96ea5a637834da655cd13e7c3daf4097c5d16e2
Louis Koh <louiskoh1@gmail.com>
2022-10-29 21:43:31 +0800

    v2 for shorten url requires login

--- script.js
+++ script.js
@@ -69,7 +69,39 @@ function shortenIT(form) {
   return false
   };
 
+  function shortenITv2(form) {
+    console.log('Domain',domainName)
+    const params = form.longURL.value
+    console.log('Params',params)
+    const endpoint = "v2/shorten_url"
+    const complete_URL = domainName + endpoint + "?long_URL=" + params
+    console.log("POST: ", complete_URL )
+    axios.post(complete_URL,{ withCredentials: true, headers})
+          .then(response => {
+                response_login = response['data']['shorten_url'];
+                console.log("OUTPUT HASH:", response_login)
+                shortItURL = domainName + response_login
+                form.longURL.value = shortItURL
+                navigator.clipboard.writeText(form.longURL.value);
+                
+                const postP = document.getElementById("postShorten")
+                postP.removeAttribute("hidden")
+                const postP_button = document.getElementById("postShortenButton")
+                postP_button.removeAttribute("hidden")
+                const string  = params + shortItURL
+                console.log(string)
+                const spacesRequired = 97 - string.length
+                postP.value = params + ":" + " ".repeat(spacesRequired-1) + shortItURL
+                navigator.clipboard.writeText(form.longURL.value);
+                form.reset()
+                })
+          .catch(error => 
+                console.error(error)
+                );
     
+    
+    return false
+    };
 
 
 function login() {
@@ -203,7 +235,6 @@ function changeForm() {
 
 function getAnalytics() {
   if (window.location.pathname=='/project4-shorten_url_FE/analytics') {
-
     const endpoint = "v1/analytics"
     const complete_URL = domainName + endpoint 
     console.log("Complete URL- analytiics", complete_URL)
