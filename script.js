3de38fd5bd9971dce4f1274dd3bd633f8ef45159
Louis Koh <louiskoh1@gmail.com>
2022-10-30 15:28:22 +0800

    added css + js

--- script.js
+++ script.js
@@ -68,39 +68,35 @@ function shortenIT(form) {
   return false
   };
 
-// function shortenITv2(form) {
-//     console.log('Domain',domainName)
-//     const params = form.longURL.value
-//     console.log('Params',params)
-//     const endpoint = "v2/shorten_url"
-//     const complete_URL = domainName + endpoint + "?long_URL=" + params
-//     console.log("POST: ", complete_URL )
-//     axios.get(complete_URL,{ withCredentials: true, headers})
-//           .then(response => {
-//                 response_login = response['data']['shorten_url'];
-//                 console.log("OUTPUT HASH:", response_login)
-//                 shortItURL = domainName + response_login
-//                 form.longURL.value = shortItURL
-//                 navigator.clipboard.writeText(form.longURL.value);
-                
-//                 const postP = document.getElementById("postShorten")
-//                 postP.removeAttribute("hidden")
-//                 const postP_button = document.getElementById("postShortenButton")
-//                 postP_button.removeAttribute("hidden")
-//                 const string  = params + shortItURL
-//                 console.log(string)
-//                 const spacesRequired = 97 - string.length
-//                 postP.value = params + ":" + " ".repeat(spacesRequired-1) + shortItURL
-//                 navigator.clipboard.writeText(form.longURL.value);
-//                 form.reset()
-//                 })
-//           .catch(error => 
-//                 console.error(error)
-//                 );
+function shortenITv2(form) {
+    console.log('Domain',domainName)
+    const params = form.longURL.value
+    console.log('Params',params)
+    const endpoint = "v2/shorten_url"
+    const complete_URL = domainName + endpoint + "?long_URL=" + params
+    console.log("POST: ", complete_URL )
+    axios.get(complete_URL,{ withCredentials: true, headers})
+          .then(response => {
+                response_login = response['data']['shorten_url'];
+                console.log("OUTPUT HASH:", response_login)
+                shortItURL = domainName + response_login
+                form.longURL.value = shortItURL
+                navigator.clipboard.writeText(form.longURL.value);
+                const postP = document.getElementById("postShorten")
+                postP.value = shortItURL
+                const preP = document.getElementById("preShorten")
+                preP.value = params
+                postP.style.backgroundColor = "#EBF5FB"
+                const postP_button = document.getElementById("postShortenButton")
+                postP_button.disabled = true
+                })
+          .catch(error => 
+                console.error(error)
+                );
     
     
-//     return false
-//     };
+    return false
+    };
 
 
 function login() {
